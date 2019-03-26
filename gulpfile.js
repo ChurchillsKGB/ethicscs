261f4ab583b712abb24971c235355a76b683e31d
Brandon <bboatright013@gmail.com>
2019-03-25 22:17:23 -0400

    take 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function() {
+          console.log("Hooray you created a gulp task");
+          });
+
+gulp.task('html', function() {
+    console.log("imagine something useful being done to your htm here");
+});
+
+gulp.task('watch', function() {
+    
+     watch('./index.html', function(){
+        gulp.start('html'); 
+     });
+});
